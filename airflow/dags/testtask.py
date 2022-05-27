16d25b29ac5263f013cd2b6d42be707f9de4219b
Marlon Saura Félix Rozindo <marlon.saura@gmail.com>
2022-05-26 23:22:19 -0300

    testtask update

--- airflow/dags/testtask.py
+++ airflow/dags/testtask.py
@@ -1,34 +1,39 @@
 # [START import_module]
-import time
-import tempfile
-import requests
+from fileinput import filename
 import pandas as pd
 from datetime import datetime
 from airflow import DAG
 from airflow.decorators import task, dag
-from os import getenv
-from os import path
-from minio import Minio
 from airflow.operators.dummy import DummyOperator
-# [END import_module]
-
+from io import BytesIO, StringIO
 from faker.extract.requestAPI import request_data_as_df
-MINIO_PORT = 9000
-HOST_URI = getenv('HOST_URI')
-BUCKET_NAME = getenv('STAGING')
-ACCESS_KEY = getenv('ACCESS_KEY')
-SECRET_KEY = getenv('SECRET_KEY')
-
+import boto3
+from botocore.client import Config
+# [END import_module]
+               
 @dag(schedule_interval = '@daily', start_date=datetime(2022,5,26), catchup = False)
 def taskflow():
 
     @task(task_id='extract')
     def extract_data_as_df() -> None:
-        df = request_data_as_df(1000)
-        file_name = f'''faker_csv_{datetime.now().strftime('%Y-%m-%d')}'''
-        df.to_csv(file_name,)
+        df = request_data_as_df('{{var.json.requested_number_of_rows}}')
 
-        return 'extract'
+        file_name = f'''faker_csv_{datetime.now().strftime('%Y-%m-%d')}'''
+        
+        s3 = boto3.resource('s3',
+                    endpoint_url='http://165.227.255.79:9000/',
+                    aws_access_key_id='myaccesskey',
+                    aws_secret_access_key='mysecretkey',
+                    config=Config(signature_version='s3v4'),
+                    region_name='us-east-1')
+
+        buffer = BytesIO()
+
+        df.to_csv(buffer, sep=",", index=False, mode="wb", encoding="UTF-8")
+        s3.Object("staging", file_name).put(Body=buffer.getvalue())
+                
+
+        return 'Successfully extracted {{var.json.requested_number_of_rows}}'
 
     extract_data_as_df()
 
